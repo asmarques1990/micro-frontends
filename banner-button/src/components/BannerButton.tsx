ee7a4536db850c0dc838eb6e5bf175382a482ecf
andre <andre.3.marques@nokia.com>
2025-02-20 09:54:14 +0000

    - Added missing files; - Removed unnecessary files;

--- banner-button/src/components/BannerButton.tsx
+++ banner-button/src/components/BannerButton.tsx
@@ -3,7 +3,7 @@ import useStore from "store/store";
 import "../index.css";
 
 export const BannerButton = () => {
-    const { clear } = useStore();
+    const { clear }: { clear: () => void } = useStore();
     return (
         <button
             className="bg-indigo-800 text-white font-bold py-2 px-4 rounded cursor-pointer"
--- banner-count/src/components/BannerCount.tsx
+++ banner-count/src/components/BannerCount.tsx
@@ -1,7 +1,7 @@
 import useStore from "store/store";
 
 export const BannerCount = () => {
-    const { count } = useStore();
+    const { count }: { count: number } = useStore();
     return (
         <>
             Count: { count }
